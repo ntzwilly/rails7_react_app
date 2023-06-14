e91eb7ae1ff73f6e1c57cd78ddc5132f1da40930
ntzwilly <ntzwilly@gmail.com>
2023-06-14 21:31:01 +0200

    create and display welcome component

--- app/javascript/react/src/components/Welcome.jsx
+++ app/javascript/react/src/components/Welcome.jsx
@@ -3,6 +3,14 @@ import * as ReactDOM from 'react-dom';
 
 const Welcome = () => {
   return (
-    <h1>Welcome to the react app application</h1>
+    <div className='container'>
+      <h1>Hello World!</h1>
+    </div>
   )
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Welcome />, document.getElementById('welcome'))
+})
+
+export default Welcome;
