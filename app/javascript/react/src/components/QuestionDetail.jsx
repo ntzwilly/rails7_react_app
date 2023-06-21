e150db47907961e06da4cb538348e195f70cc56d
ntzwilly <ntzwilly@gmail.com>
2023-06-21 22:18:41 +0200

    remove comments

--- app/javascript/react/src/components/QuestionDetail.jsx
+++ app/javascript/react/src/components/QuestionDetail.jsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import { useState } from 'react';
-
 class QuestionDetail extends React.Component {
   constructor(props) {
     super(props)
@@ -78,36 +76,5 @@ class QuestionDetail extends React.Component {
   }
 
 }
-
-
-// const QuestionDetail = (props) => {
-
-//   const[likeCount, setLikeCount] = useState(0);
-//   const handleLike = () => setLikeCount(likeCount + 1)
-//   const handleDislike = () => likeCount > 0 && setLikeCount(likeCount - 1)
-
-//   return (
-//     <div className='card rounded-0 mt-3'>
-//       <div className='card-body'>
-//         <h3 className='card-title'>{props.question.title}</h3>
-//         <p className='lead'>
-//           <span className='badge bg-primary'>{props.question.tag}</span>
-//         </p>
-//         <button className='btn btn-primary mt-1' 
-//                 onClick={handleLike}>
-//           Like
-//         </button>
-//         {
-//           likeCount > 0 ? 
-//           <span className='badge bg-danger'>{likeCount}</span> : ''
-//         }
-//         <button className='btn btn-warning mt-1' 
-//                 onClick={handleDislike}>
-//           Dislike
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default QuestionDetail;
