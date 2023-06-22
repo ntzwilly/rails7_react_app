a687096bc09fa7e3fab305e59fd0eb6a16e75f90
ntzwilly <ntzwilly@gmail.com>
2023-06-22 19:20:12 +0200

    filter values with dropdown

---
+++ app/javascript/react/src/components/EmptyQuestionMessage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const EmptyQuestionMessage = (props) => {
+  return (
+    <div className="alert alert-warning alert-dismissible fade show" role="alert">
+      <strong>OOPs!</strong> No question found with the tag: {props.tagName}. Please select another option from the list
+
+      <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  )
+}
+
+export default EmptyQuestionMessage;
+
+
--- app/javascript/react/src/components/QuestionList.jsx
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import QuestionDetail from './QuestionDetail';
+import EmptyQuestionMessage from './EmptyQuestionMessage';
 
 const QuestionList = () => {
-
+  const questionTags = [
+    { label: 'All', value: 0},
+    { label: 'Ruby', value: 1},
+    { label: 'Rails', value: 2},
+    { label: 'React', value: 3},
+    { label: 'Bootstrap', value: 4},
+    { label: 'Javascript', value: 5},
+  ]
   const [questionList, setQuestionList] = useState([]);
+  const [selectedOption, setSelectedOption] = useState(questionTags[0].value)
+
+
   const questionUrl = 'http://localhost:3000/api/v1/questions'
 
   const fetchQuestionList = () => {
@@ -13,19 +24,46 @@ const QuestionList = () => {
       .then(data => {
         console.log(data)
         setQuestionList(data)
-      })
+    })
   }
   
   useEffect(() => {
     fetchQuestionList()
   }, []);
 
+  const updateSelectedItem = (event) => {
+    setQuestionList([])
+    setSelectedOption(event.target.value)
+    fetch(questionUrl + `?tags=${questionTags[event.target.value].label}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      setQuestionList(data)
+  })
+  }
+
   return (
     <div className='row'>
       <div className='col-lg-10 mx-auto'>
-        {questionList.map((question) => 
-          <QuestionDetail question={question} key={question.id} /> 
-        )}
+        <p className='lead fw-bold'>Filter questions by Tags</p>
+        <select className='form-select form-select-lg' 
+                value={selectedOption}
+                onChange={event => updateSelectedItem(event)}
+        >
+          {
+            questionTags.map(tag => (
+              <option key={tag.value} value={tag.value}>{tag.label}</option>
+            ))
+          }
+        </select>
+
+        {
+          questionList.length > 0 ?
+          questionList.map((question) => 
+            <QuestionDetail question={question} key={question.id} /> 
+          ) : <EmptyQuestionMessage tagName={questionTags[selectedOption].label} />
+        }
+  
       </div>
     </div>
   )
