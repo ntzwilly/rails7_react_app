f0bc70d9930e14e8452a288da960d627c9b311e9
ntzwilly <ntzwilly@gmail.com>
2023-06-27 22:39:37 +0200

    fix bootstrap close button bug

--- app/javascript/react/src/components/EmptyQuestionMessage.jsx
+++ app/javascript/react/src/components/EmptyQuestionMessage.jsx
@@ -5,7 +5,7 @@ const EmptyQuestionMessage = (props) => {
     <div className="alert alert-warning alert-dismissible fade show" role="alert">
       <strong>OOPs!</strong> No question found with the tag: {props.tagName}. Please select another option from the list
 
-      <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+      <button type="button" className="close" data-bs-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
       </button>
     </div>
